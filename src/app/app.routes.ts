7f5a7ede766748970eacf4b4cc98dc4d8825dc52
moritztheil <moritz@startup-werk.de>
2024-10-18 15:49:23 +0200

    :sparkles: books-page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { BooksPageComponent } from './pages/books-page/books-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: BooksPageComponent,
+  },
+];
